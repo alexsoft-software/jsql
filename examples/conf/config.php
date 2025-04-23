46c12f13249b81c927fe624561cb5b0f254cc5d5
Alexsoft Software <4207994+alexsoft-software@users.noreply.github.com>
2025-04-23 13:45:03 +0300

    Examples

---
+++ examples/conf/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'jsql' => [
+        'config_path' => '/root/path/conf/config.json', 
+        'users_path' => '/root/path/conf/users.json',
+        'databases_root_path' => '/root/path/jsql_db',      
+    ]
+];
+?>
